2e87a49a4ea8786296a9e783c51a4e04dc40971d
kthatoto <kthatoto@gmail.com>
2019-06-18 20:34:38 +0900

    Name daemon process by self and kill it

--- lib/termworld/cli.rb
+++ lib/termworld/cli.rb
@@ -14,18 +14,20 @@ module Termworld
       3.times { |i| $db[:users].insert(name: "Sequel:#{i}", price: 0) }
 
       puts "Started!"
+      Process.setproctitle("termworld_daemon")
       Process.daemon(true, false) # (nochdir, noclose)
       loop do
         $db[:users].update(price: Sequel[:price] + 1)
         break if @killed
-        sleep 3
+        sleep 1
       end
       DB.stop
     end
 
     desc "stop", "Stop game client."
     def stop
-      `ps aux | grep termworld | grep -v grep | awk '{print $2}' | xargs kill`
+      `ps aux | grep termworld_daemon | grep -v grep | awk '{print $2}' | xargs kill`
+      puts "Stopped!"
     end
   end
 end
