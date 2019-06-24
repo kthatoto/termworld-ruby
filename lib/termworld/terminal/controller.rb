369be28fa0e9630127b8556cdac7850a58a1fd8b
kthatoto <kthatoto@gmail.com>
2019-06-25 01:21:41 +0900

    Add map chip

--- lib/termworld/terminal/controller.rb
+++ lib/termworld/terminal/controller.rb
@@ -10,7 +10,7 @@ module Termworld
       end
 
       def run
-        field = TermCanvas::Canvas.new(x: 0, y: 0, w: 10, h: 10)
+        field = TermCanvas::Canvas.new(x: 0, y: 0, w: TermCanvas.width, h: TermCanvas.height)
 
         loop do
           key = TermCanvas.gets
@@ -20,16 +20,17 @@ module Termworld
           end
           field.clear
 
+          # background = TermCanvas::Rect.new(
+          #   x: 0, y: 0, width: 10, height: 10,
+          #   background_color: {r: 200, b: 200, g: 800},
+          # )
+          # field.rect(background)
           player = TermCanvas::Rect.new(
             x: field.centerx, y: field.centery, width: 2, height: 1,
-            background_color: {r: 200, b: 200, g: 700},
+            background_color: {r: 200, b: 200, g: 200},
           )
           field.rect(player)
-          background = TermCanvas::Rect.new(
-            x: 0, y: 0, width: 10, height: 10,
-            background_color: {r: 200, b: 200, g: 800},
-          )
-          field.rect(background)
+
           field.update
           sleep 1
         end
