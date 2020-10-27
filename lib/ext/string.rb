5ca5adf8d41fa0912547cbda792525ce2d1b6a29
kthatoto <kthatoto@gmail.com>
2020-10-28 02:58:57 +0900

    Add ext for string color

---
+++ lib/ext/string.rb
@@ -0,0 +1,14 @@
+class String
+  def reden
+    "\e[31m#{self}\e[0m"
+  end
+  def greenen
+    "\e[32m#{self}\e[0m"
+  end
+  def yellowen
+    "\e[33m#{self}\e[0m"
+  end
+  def bluen
+    "\e[34m#{self}\e[0m"
+  end
+end
