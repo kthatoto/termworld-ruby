7a3ad81141eec82aa342a503a8bca255cdadeb6e
kthatoto <kthatoto@gmail.com>
2020-10-29 04:51:11 +0900

    Make multiple canvases

--- lib/termworld/game/terminal/controller.rb
+++ lib/termworld/game/terminal/controller.rb
@@ -8,45 +8,22 @@ module Termworld
     class Controller
       def initialize(user)
         @user = user
+        @map = Resources::Maps::Town.new
+        @field = TermCanvas::Canvas.new(x: 0, y: 0, w: TermCanvas.width - 30, h: TermCanvas.height - 10)
+        @meta_canvas = TermCanvas::Canvas.new(x: @field.width, y: 0, w: 30, h: TermCanvas.height)
+        @status_canvas = TermCanvas::Canvas.new(x: 0, y: @field.height, w: TermCanvas.width - 30, h: 10)
+        @canvases = [@field, @meta_canvas, @status_canvas]
       end
 
       def run
-        field = TermCanvas::Canvas.new(x: 0, y: 0, w: TermCanvas.width, h: TermCanvas.height)
-
-        map = Resources::Maps::Town.new
         loop do
-          @user.bind_local_by_name
-          users = Models::User.all_local.reject { |user| user.name == @user.name }
-
           break if handle_keys == :break
-          field.clear
+          @users = Models::User.all_local.reject {|user| user.name == @user.name}
+          @canvases.each(&:clear)
 
-          height = field.height
-          width = field.width / 2 - 1
-          height.times do |y|
-            width.times do |x|
-              abs_position = {
-                x: @user.positionx - (width / 2) + x,
-                y: @user.positiony - (height / 2) + y,
-              }
-              user = users.find { |u|
-                u.positionx == abs_position[:x] && u.positiony == abs_position[:y]
-              }
-              if user
-                player_chip = Resources::Chip.new(x: x * 2 + 1, y: y, key: "pl")
-                field.rect(player_chip.rect)
-                next
-              end
-              next if abs_position.any? { |_, v| v < 0 }
-              next if (chip = map.get_chip(abs_position)).nil?
-              field.rect(chip.rect.position_override(x: x * 2 + 1, y: y))
-            end
-          end
+          draw_field
 
-          player_chip = Resources::Chip.new(x: field.centerx, y: field.centery, key: "pl")
-          field.rect(player_chip.rect)
-
-          field.update
+          @canvases.each(&:update)
           sleep 0.05
         end
         TermCanvas.close
@@ -68,6 +45,34 @@ module Termworld
           when ?l
             @user.move(:right)
           end
+        end
+
+        def draw_field
+          height = @field.height
+          width = @field.width / 2 - 1
+
+          height.times do |y|
+            width.times do |x|
+              abs_position = {
+                x: @user.positionx - (width / 2) + x,
+                y: @user.positiony - (height / 2) + y,
+              }
+              user = @users.find { |u|
+                u.positionx == abs_position[:x] && u.positiony == abs_position[:y]
+              }
+              if user
+                player_chip = Resources::Chip.new(x: x * 2 + 1, y: y, key: "pl")
+                @field.rect(player_chip.rect)
+                next
+              end
+              next if abs_position.any? { |_, v| v < 0 }
+              next if (chip = @map.get_chip(abs_position)).nil?
+              @field.rect(chip.rect.position_override(x: x * 2 + 1, y: y))
+            end
+          end
+
+          player_chip = Resources::Chip.new(x: @field.centerx, y: @field.centery, key: "pl")
+          @field.rect(player_chip.rect)
         end
     end
   end
