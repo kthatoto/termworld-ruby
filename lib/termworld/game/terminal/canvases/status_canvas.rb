01a3859f3ea34cf53bc50043ee44507fd495d643
kthatoto <kthatoto@gmail.com>
2020-10-29 12:28:54 +0900

    Display some status

--- lib/termworld/game/terminal/canvases/status_canvas.rb
+++ lib/termworld/game/terminal/canvases/status_canvas.rb
@@ -15,17 +15,43 @@ module Termworld
         @canvas.background(BACKGROUND_COLOR)
 
         level_y = 1
-        text(x: 2, y: level_y, body: "Lv. #{@store.user.level}")
+        level_width = 25
+        text(x: 1, y: level_y, body: "Lv. #{@store.user.level}")
         text(x: 1, y: level_y + 1, body: "EXP")
-        text(x: 5, y: level_y + 1, body: "▅" * 20, fg_color: BLACK)
-        text(x: 5, y: level_y + 1, body: "▅" * (@store.user_exp_percentage * 20), fg_color: BLUE)
+        text(x: 5, y: level_y + 1, body: "▅" * level_width, fg_color: BLACK)
+        text(x: 5, y: level_y + 1, body: "▅" * (@store.user_exp_percentage * level_width), fg_color: BLUE)
         text(x: 5, y: level_y + 2, body: @store.user_exp_text)
 
         hp_y = 5
+        hp_width = 25
         text(x: 2, y: hp_y, body: "HP")
-        rect(x: 5, y: hp_y, width: 20, height: 1, bg_color: BLACK)
-        rect(x: 5, y: hp_y, width: @store.user_hp_percentage * 20, height: 1, bg_color: Termworld::Canvas.const_get(@store.user_hp_color))
+        rect(x: 5, y: hp_y, width: hp_width, height: 1, bg_color: BLACK)
+        rect(x: 5, y: hp_y, width: @store.user_hp_percentage * hp_width, height: 1, bg_color: Termworld::Canvas.const_get(@store.user_hp_color))
         text(x: 5, y: hp_y + 1, body: @store.user_hp_text)
+
+        status_x = 35
+        status_y = 2
+        text(x: status_x, y: status_y, body: "Status")
+        text(x: status_x, y: status_y + 2, body: "ATK. #{@store.user.attack_power}")
+        text(x: status_x, y: status_y + 3, body: "DEF. #{@store.user.deffensive_power}")
+        text(x: status_x, y: status_y + 4, body: "AGI. ???")
+        text(x: status_x, y: status_y + 5, body: "INT. ???")
+        text(x: status_x, y: status_y + 6, body: "LUK. ???")
+
+        equipment_x = 55
+        equipment_y = 2
+        text(x: equipment_x, y: equipment_y, body: "Equipment")
+        text(x: equipment_x, y: equipment_y + 2, body: "Head: ---")
+        text(x: equipment_x, y: equipment_y + 3, body: "Body: ---")
+        text(x: equipment_x, y: equipment_y + 4, body: " Arm: ---")
+        text(x: equipment_x, y: equipment_y + 5, body: " Leg: ---")
+
+        ability_x = 75
+        ability_y = 2
+        text(x: ability_x, y: ability_y, body: "Ability")
+        text(x: ability_x, y: ability_y + 2, body: "Slot1. ---")
+        text(x: ability_x, y: ability_y + 3, body: "Slot2. ---")
+        text(x: ability_x, y: ability_y + 4, body: "Slot3. ---")
       end
     end
   end
