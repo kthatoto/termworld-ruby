a5baac9026da70bb864e08f7a69cdb0910854c4e
kthatoto <kthatoto@gmail.com>
2021-06-26 03:29:11 +0900

    Fix host

--- lib/termworld/config.rb
+++ lib/termworld/config.rb
@@ -3,5 +3,5 @@ module Termworld
   PROCESS_NAME = 'termworld_daemon'
   DATABASE_NAME = 'termworld.db'
   CREDENTIAL_FILE_NAME = 'credential'
-  API_ENDPOINT = 'http://0.0.0.0:3000'
+  API_ENDPOINT = 'http://host.docker.internal:3000'
 end
